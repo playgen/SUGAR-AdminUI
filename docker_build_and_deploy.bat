18328cc1c39bb57fb961031673e7815a80a066c6
BAYMAX\Felix <felix@playgen.com>
2018-08-10 11:35:32 +0100

    update to v2 for sugar jam

--- docker_build_and_deploy.bat
+++ docker_build_and_deploy.bat
@@ -1,2 +1,2 @@
-docker build . -t sugar-admin
-docker run --name sugar-admin sugar-admin
+docker build . -t sugar-admin-v2
+docker run --name sugar-admin-v2 sugar-admin-v2
--- docker_build_and_deploy_map_port.sh
+++ docker_build_and_deploy_map_port.sh
@@ -1,2 +1,2 @@
-docker build . -t sugar-admin
-docker run --name sugar-admin -p 59440:4200 -d sugar-admin
+docker build . -t -v2
+docker run --name sugar-admin-v2 -p 59440:4200 -d sugar-admin-v2
