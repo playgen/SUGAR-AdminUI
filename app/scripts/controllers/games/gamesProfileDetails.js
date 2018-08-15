6ce0b58587f247155b895330214ee4c566fe3294
BAYMAX\Felix <felix@playgen.com>
2018-08-15 15:09:02 +0100

    add ability to get game seed from the admin panel, useful for migrating games

--- app/scripts/controllers/games/gamesProfileDetails.js
+++ app/scripts/controllers/games/gamesProfileDetails.js
@@ -1,6 +1,6 @@
 angular.module('sgaAdminApp').controller('GamesProfileDetailsCtrl', [
-	'$scope', '$stateParams', '$location', 'modalManager', 'GamesApi',
-	function($scope, $stateParams, $location, modalManager, GamesApi) {
+	'$scope', '$stateParams', '$location', 'modalManager', 'GamesApi', 'LeaderboardsApi', 'AchievementsApi', 'SkillsApi',
+	function($scope, $stateParams, $location, modalManager, GamesApi, LeaderboardsApi, AchievementsApi, SkillsApi) {
 
 		$scope.itemId = $stateParams.itemId;
 
@@ -8,6 +8,10 @@ angular.module('sgaAdminApp').controller('GamesProfileDetailsCtrl', [
 
 		$scope.isEditing = [];
 		$scope.EditButtonText = [];
+
+    $scope.gameName = "";
+    $scope.showGameSeed = false;
+    $scope.gameSeed = {};
 
 		$scope.init = function()
 		{
@@ -30,7 +34,11 @@ angular.module('sgaAdminApp').controller('GamesProfileDetailsCtrl', [
             $scope.items[i].value = $scope.item[keys[i]];
 
             $scope.isEditing[i] = false;
-						$scope.EditButtonText[i] = "Edit";
+            $scope.EditButtonText[i] = "Edit";
+            if (keys[i] == 'name')
+            {
+              $scope.gameName = $scope.items[i].value;
+            }
           }
 				}
 			});
@@ -73,6 +81,73 @@ angular.module('sgaAdminApp').controller('GamesProfileDetailsCtrl', [
 		{
 			$scope.isEditing[index] = !$scope.isEditing[index];
 			$scope.EditButtonText[index] = $scope.isEditing[index] ? "Cancel" : "Edit";
-		}
-	}
+    }
+    $scope.ShowGameSeed = function()
+    {
+      var gameSeed = {};
+      // Game Name
+      gameSeed.game = $scope.gameName;
+
+      // Achievements
+      AchievementsApi['achievements'].list($scope.itemId).then(function(res) {
+				if (res.status === 200 && res.data['response'] != null) {
+					gameSeed.achievements = res.data['response'];
+          for (var i=0; i<gameSeed.achievements.length; i++)
+          {
+            delete gameSeed.achievements[i].id;
+            gameSeed.achievements[i].gameId = null;
+            for (var j=0; j<gameSeed.achievements[i].evaluationCriterias.length; j++)
+            {
+              delete gameSeed.achievements[i].evaluationCriterias[j].id;
+            }
+            for (var k=0; k<gameSeed.achievements[i].rewards.length; k++)
+            {
+              delete gameSeed.achievements[i].rewards[k].id;
+            }
+          }
+        }
+
+        // Leaderboards
+        LeaderboardsApi['leaderboard'].list($scope.itemId).then(function(res){
+          if (res.status === 200 && res.data['response'] != null) {
+            gameSeed.leaderboards = res.data['response'];
+            for (var i=0; i<gameSeed.leaderboards.length; i++)
+            {
+              delete gameSeed.leaderboards[i].id;
+              gameSeed.leaderboards[i].gameId = null;
+
+            }
+          }
+
+          // Skills
+          SkillsApi['skills'].listSkills($scope.itemId).then(function(res) {
+            if (res.status === 200 && res.data['response'] != null) {
+              gameSeed.skills = res.data['response'];
+              for (var i=0; i<gameSeed.skills.length; i++)
+              {
+                delete gameSeed.skills[i].id;
+                gameSeed.skills[i].gameId = null;
+
+                for (var j=0; j<gameSeed.skills[i].evaluationCriterias.length; j++)
+                {
+                  delete gameSeed.skills[i].evaluationCriterias[j].id;
+                }
+                for (var k=0; k<gameSeed.skills[i].rewards.length; k++)
+                {
+                  delete gameSeed.skills[i].rewards[k].id;
+                }
+              }
+            }
+
+            $scope.gameSeed = gameSeed;
+            $scope.showGameSeed = true
+          });
+        });
+      });
+    }
+    $scope.hideGameSeed = function()
+    {
+      $scope.showGameSeed = false;
+    }
+  }
 ]);
